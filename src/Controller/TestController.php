e341c723841ce20ee984f766aba9337959a374a7
kacimi <k2acim31@gmail.com>
2020-12-13 16:51:27 +0100

    forms + footer + entity

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,29 +2,38 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Form\ClickTypeFormType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TestController extends AbstractController
 {
     /**
-     * @Route("/test", name="test")
+     * @Route("/", name="index")
      */
     public function index(): Response
     {
-        return $this->render('test/index.html.twig', [
-            'controller_name' => 'TestController',
+        return $this->render('index.html.twig', [
+            'controller_name' => "Salut, je m'appel HAMED!",
         ]);
     }
 
     /**
-     * @Route("/", name="click")
+     * @Route("/click", name="click")
      */
     public function click() {
-        return $this->render('home.html.twig', [
-            'title' => "Bonjour bienvenue dans la page d'accueil !",
-            'age' => 15,
+        $user = new User();
+        $form = $this->createForm(ClickTypeFormType::class,$user);
+
+        if ($form->isSubmitted() && $form->isValide()) {
+            dd($form);
+        }
+        return $this->render('click/home.html.twig', [
+            'title' => "Greeting and Welcome !",
+            'title2' => "you wanna play a game ?",
+            'form' => $form->createView(),
         ]);
     }
 }
